ae2ceea5db310c31411f7b9eb60da2efe0d115cf
venghatesh <venghatesh@Venghateshs-MacBook-Air.local>
2018-09-24 22:12:25 +0530

    handled bug related to book shelf not set to none in search page

--- src/components/BooksListDetail.js
+++ src/components/BooksListDetail.js
@@ -12,7 +12,7 @@ const BooksListDetail = ({ book, handleBookShelf }) => {
                         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageThumb})` }}></div>
                             <div className="book-shelf-changer">
                                 <select onChange={e => handleBookShelf(book, e.target.value)} value={book.shelf} >
-                                    <option value="none" disabled>Move to...</option>
+                                    <option value="Move to" disabled>Move to...</option>
                                     <option value="currentlyReading">Currently Reading</option>
                                     <option value="wantToRead">Want to Read</option>
                                     <option value="read">Read</option>
